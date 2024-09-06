7dcc530ff40cc6bcb403e33821caaf9aaadc1214
vitoffer <ms.titoff1011@yandex.ru>
2024-09-06 23:18:22 +0300

    init template for app

---
+++ frontend/src/components/TheHeader.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<header>Header</header>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<main>Main page</main>
+</template>
+
+<style scoped></style>
