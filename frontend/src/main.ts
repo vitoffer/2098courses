ab2f1ab253e465fec856f496778ef36cde8177ad
vitoffer <ms.titoff1011@yandex.ru>
2024-09-08 03:15:48 +0300

    add adaptive to filter section

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import 'primeicons/primeicons.css'
 import '@/assets/styles/main.css'
 
 import { createApp } from 'vue'
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, watchEffect } from "vue"
+import { nextTick, onMounted, ref } from "vue"
 import MultiSelect from "primevue/multiselect"
-import { useFetch } from "@vueuse/core"
 import Select from "primevue/select"
 import InputMask from "primevue/inputmask"
-import Slider from "primevue/slider"
 
-// const filters = [
-// 	"Направленность",
-// 	"Адрес",
-// 	"Преподаватель",
-// 	"Возраст",
-// 	"Цена",
-// 	"Дни недели",
-// 	"Время",
-// ]
+const isFilterSectionShown = ref(window.innerWidth >= 768)
 
 const searchTextModel = ref()
 
@@ -58,17 +48,40 @@ onMounted(async () => {
 	const responseTeachers = await fetch(filterTeachersOptionsFetchUrl)
 	filterTeachersOptions.value = await responseTeachers.json()
 })
+
+async function checkMultiSelectItems() {
+	await nextTick()
+
+	const pcFilterContainer = document.querySelector('[data-pc-name="pcfilter"]')
+
+	if (pcFilterContainer) {
+		pcFilterContainer.classList.add("base-input")
+	}
+}
 </script>
 
 <template>
 	<main class="container">
-		<input
-			type="text"
-			placeholder="Начните вводить название кружка для поиска"
-			class="search-bar base-input"
-			v-model="searchTextModel"
-		/>
-		<section class="filter filter">
+		<section class="search-wrapper">
+			<button
+				class="filter__button"
+				@click="isFilterSectionShown = !isFilterSectionShown"
+			>
+				<i class="pi pi-filter"></i>
+			</button>
+			<input
+				type="text"
+				placeholder="Поиск по названию"
+				class="search-bar base-input"
+				v-model="searchTextModel"
+			/>
+		</section>
+		<section
+			class="filter"
+			:style="{
+				display: isFilterSectionShown ? 'block' : 'none',
+			}"
+		>
 			<p class="filter__title">Фильтры:</p>
 			<ul class="filter__list filter-list">
 				<li class="filter-list__item filter-item">
@@ -81,6 +94,7 @@ onMounted(async () => {
 						filter
 						:max-selected-labels="2"
 						selectedItemsLabel="Выбрано {0} элементов"
+						@focus="checkMultiSelectItems"
 					/>
 				</li>
 				<li>
@@ -93,6 +107,7 @@ onMounted(async () => {
 						filter
 						:max-selected-labels="2"
 						selectedItemsLabel="Выбрано {0} элементов"
+						@focus="checkMultiSelectItems"
 					/>
 				</li>
 				<li>
@@ -105,6 +120,7 @@ onMounted(async () => {
 						filter
 						:max-selected-labels="1"
 						selectedItemsLabel="Выбрано {0} элементов"
+						@focus="checkMultiSelectItems"
 					/>
 				</li>
 				<li>
@@ -154,10 +170,29 @@ onMounted(async () => {
 	margin: 0 auto;
 }
 
-.search-bar {
-	display: block;
-	width: 600px;
-	margin: 0 auto;
+.search {
+	&-wrapper {
+		display: flex;
+		justify-content: center;
+		gap: 8px;
+		padding: 0 16px;
+	}
+
+	&-bar {
+		display: block;
+		width: 600px;
+		margin: 0 auto;
+
+		@media (max-width: 767px) {
+			width: 80vw;
+			margin: 0;
+		}
+
+		@media (max-width: 480px) {
+			width: 90vw;
+			padding-inline: 8px;
+		}
+	}
 }
 
 .search-bar::placeholder {
@@ -165,13 +200,41 @@ onMounted(async () => {
 }
 
 .filter {
+	@media (max-width: 767px) {
+		display: flex;
+		flex-direction: column;
+		margin-top: 8px;
+		padding-block: 8px;
+		background-color: var(--border-light-gray);
+	}
+
+	&__button {
+		display: none;
+		padding: 8px;
+		line-height: 0;
+		background-color: var(--blue-primary);
+		border-radius: 10px;
+
+		.pi-filter {
+			font-size: 1rem;
+			color: var(--text-white);
+		}
+
+		@media (max-width: 767px) {
+			display: block;
+		}
+	}
+
 	&__title {
 		margin-block: 8px;
-
 		font-size: 0.875rem;
 		line-height: 1.4;
 		text-align: center;
 		color: var(--text-black);
+
+		@media (max-width: 767px) {
+			margin: 0;
+		}
 	}
 
 	&__list {
@@ -220,10 +283,6 @@ $active-border: solid 1px var(--blue-primary) !important;
 	transition-duration: 0.2s;
 }
 
-.filter-item__input:focus {
-	border: solid 1px var(--blue-primary) !important;
-}
-
 .filter-item__input {
 	width: 250px;
 }
@@ -244,14 +303,28 @@ $active-border: solid 1px var(--blue-primary) !important;
 	}
 }
 
-.p-multiselect-overlay {
-	.p-checkbox-checked .p-checkbox-box {
-		background-color: var(--blue-primary) !important;
-		border: none;
+.p-multiselect {
+	&-overlay {
+		max-width: 100%;
 
-		.p-checkbox-icon * {
-			color: var(--text-white) !important;
+		.p-checkbox-checked .p-checkbox-box {
+			background-color: var(--blue-primary) !important;
+			border: none !important;
+
+			.p-checkbox-icon * {
+				color: var(--text-white) !important;
+			}
 		}
+	}
+
+	&-header {
+		padding-inline: 8px !important;
+	}
+
+	&-option > span {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
 	}
 }
 </style>
